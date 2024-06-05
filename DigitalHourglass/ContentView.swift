e40c0a854431a7e6b9fe0d83fc596d2d68682b9d
Naoki Takahashi / 電電猫猫 <43579605+nyanko3141592@users.noreply.github.com>
2024-06-05 17:14:58 +0900

    Initial Commit

---
+++ DigitalHourglass/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DigitalHourglass
+//
+//  Created by 高橋直希 on 2024/06/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DigitalHourglass/DigitalHourglassApp.swift
@@ -0,0 +1,17 @@
+//
+//  DigitalHourglassApp.swift
+//  DigitalHourglass
+//
+//  Created by 高橋直希 on 2024/06/05.
+//
+
+import SwiftUI
+
+@main
+struct DigitalHourglassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
